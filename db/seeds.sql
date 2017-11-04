9549d842ea9ad13f4aac0259fbc4415069f0897f
Anna Ly <annatly2@gmail.com>
2017-11-04 16:21:07 -0700

    nov 4

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ("In N Out Burger");
+INSERT INTO burgers (burger_name) VALUES ("Cheese Burger");
+INSERT INTO burgers (burger_name) VALUES ("Hamburger without pickles");
